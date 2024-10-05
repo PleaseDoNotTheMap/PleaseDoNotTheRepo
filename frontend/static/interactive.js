948beace01e47d47f9386ff78db6f1ba841da6c5
Leo Qi <leo@leozqi.com>
2024-10-05 17:19:45 -0400

    Actually actually merge

--- frontend/static/interactive.js
+++ frontend/static/interactive.js
@@ -1,13 +1,10 @@
 import Globe from 'globe.gl';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as THREE from '//unpkg.com/three/build/three.module.js';
-
-const globe = Globe({animateIn:false});
 
 import {MTLLoader} from 'three/addons/loaders/MTLLoader.js';
 import {OBJLoader} from 'three/addons/loaders/OBJLoader.js';
 
-const globe = Globe();
+const globe = Globe({animateIn:false});
 const markerSvg = `<svg viewBox="-4 0 36 36">
   <path fill="currentColor" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
   <circle fill="black" cx="14" cy="14" r="7"></circle>
