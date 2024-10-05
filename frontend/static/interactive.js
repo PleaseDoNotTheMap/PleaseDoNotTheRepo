4f39fd90b9b6bf10e4b5d95ed19fc91c4f566a67
Leo Qi <leo@leozqi.com>
2024-10-05 16:01:20 -0400

    Add satellite

--- frontend/static/interactive.js
+++ frontend/static/interactive.js
@@ -4,15 +4,19 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const globe = Globe();
 
-globe(document.getElementById('globe'), {
-        // Globe options
-})
-  .globeImageUrl("http://unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
+globe(document.getElementById('globe'), {})
+  .globeImageUrl("https://unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
+  .backgroundImageUrl('https://unpkg.com/three-globe/example/img/night-sky.png')
   .polygonCapColor(feat => 'rgba(200, 0, 0, 0.6)')
   .polygonSideColor(() => 'rgba(0, 100, 0, 0.05)')
   .polygonAltitude(0.06)
   .polygonStrokeColor(() => '#111')
   .lineHoverPrecision(0)
+      .objectLat('lat')
+      .objectLng('lng')
+      .objectAltitude('alt')
+      .objectFacesSurface(false)
+      .objectLabel('name');
 
 const getScenes = async function() {
   fetch("/static/small.geojson")
@@ -26,14 +30,16 @@ const renderSatellite = function() {
   let loader = new GLTFLoader();
 
   loader.load('/static/satellite.glb', function(gltf) {
-    scene.add(gltf.scene);
+    globe.objectThreeObject(gltf.scene);
+    globe.objectsData([{
+      lat: 0,
+      lng: 0,
+      alt: 1,
+      lbl: 'Satellite',
+    }]);
   }, undefined, function(error) {
     console.error(error);
   });
-
-  globe.objectsData([
-    { type: 'satellite', lat: 0, lng: 0, altitude: 10}
-  ]);
 }
 
 renderSatellite();
