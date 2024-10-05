80481f589c313bcf912cef98a25566109b64d6e3
Leo Qi <leo@leozqi.com>
2024-10-05 12:35:37 -0400

    AHHH comma
    Fixes
    
    Interactive

--- static/interactive.js
+++ static/interactive.js
@@ -4,17 +4,17 @@ globe(document.getElementById('glb-container'), {
         // Globe options
 })
   .globeImageUrl("http://unpkg.com/three-globe/example/img/earth-blue-marble.jpg")
-  .polygonCapColor(feat => 'rgba(200, 0 0, 0.6)')
+  .polygonCapColor(feat => 'rgba(200, 0, 0, 0.6)')
   .polygonSideColor(() => 'rgba(0, 100, 0, 0.05)')
   .polygonAltitude(0.06)
   .polygonStrokeColor(() => '#111')
   .lineHoverPrecision(0)
 
 const getScenes = async function() {
-  fetch("/static/simple.geojson")
+  fetch("/static/small.geojson")
     .then(response => response.json())
     .then(data => {
-      globe.polygonGeoJsonGeometry(data);
+      globe.polygonsData(data);
     });
 }
 
--- tools/normalgeo.py
+++ tools/normalgeo.py
@@ -1,25 +1,26 @@
 import json
-
 def main():
     with open("./scenes.geojson", "r") as f:
         data = json.load(f)
-
         to_write = []
-        with open("./simple.geojson", "w") as f:
+        with open("./scenes.geojson", "w") as f:
             for feature in data["features"]:
-
                 if "type" not in feature:
                     continue
                 if "geometry" not in feature:
                     continue
                 if "type" not in feature["geometry"]:
                     continue
-
                 if feature["geometry"]["type"] == "Polygon":
-                    to_write.append(feature["geometry"])
+                    to_write.append({
+                        "type": feature["type"],
+                        "geometry": feature["geometry"]
+                    })
         
         with open("./simple.geojson", "w") as f:
-            json.dump(to_write, f)
-
+            json.dump({
+                "type": "FeatureCollection",
+                "features": to_write
+            }, f)
 if __name__ == "__main__":
     main()
