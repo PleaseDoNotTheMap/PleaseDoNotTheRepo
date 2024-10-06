3d0e51e4e1bc3521ad19195b7087b3e78210652b
Amy <amy.hua.ca@gmail.com>
2024-10-06 11:36:15 -0400

    minor documentation

--- backend/geojson/converter.py
+++ backend/geojson/converter.py
@@ -1,3 +1,6 @@
+#converts CSV file to GeoJSON file
+#change csv_file and geojson_file to corresponding paths
+
 import csv
 import json
 
@@ -9,6 +12,7 @@ def csv_to_geojson(csv_file, geojson_file):
 
     with open(csv_file, 'r') as f:
         reader = csv.DictReader(f)
+        #iterate through csv file
         for row in reader:
             feature = {
                 "type": "Feature",
@@ -35,7 +39,7 @@ def csv_to_geojson(csv_file, geojson_file):
     with open(geojson_file, 'w') as f:
         json.dump(geojson, f, indent=4)
 
-# Example usage
+
 csv_file = "backend\geojson\\test.csv"
 geojson_file = "output.geojson"
 csv_to_geojson(csv_file, geojson_file)
